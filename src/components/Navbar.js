6445bdfa3b541d1b4a1a9c55972c807032a6904b
Itisafeature <thebigdebugger@gmail.com>
2020-09-07 00:33:45 -0400

    styling for signup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,21 @@ const Navbar = () => {
   return (
     <nav className="signup-navigation-container">
       <div className="signup-welcome-container">
-        <img
-          className="logo-img"
-          src="project-tracker-logo.png"
-          alt="company logo"
-        ></img>
+        <a href="#">
+          <img
+            className="logo-img"
+            src="project-tracker-logo.png"
+            alt="company logo"
+          ></img>
+        </a>
+        <a className="learn-more" href="#">
+          Learn More
+        </a>
+      </div>
+      <div className="signup-form-navigation">
+        <a className="signin-link" href="#">
+          Sign In
+        </a>
       </div>
     </nav>
   );
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -17,7 +17,32 @@ const Signup = () => {
           </p>
         </div>
       </div>
-      <div className="signup-information"></div>
+      <div className="signup-information">
+        <div className="signup-card">
+          <h2 className="signup-card-header">Signup</h2>
+          <span className="signin-link-span signup-card-item">
+            or <a href="#">Signin</a>
+          </span>
+          <form className="signup-form">
+            <input
+              type="text"
+              className="signup-card-item"
+              id="signup-email"
+              placeholder="email"
+            />
+            <input
+              type="password"
+              className="signup-card-item"
+              id="signup-password"
+              placeholder="password"
+            />
+            <button className="btn signup-form-btn" type="submit">
+              Sign Up
+            </button>
+          </form>
+          <button className="btn google-signin-btn">Sign in with Google</button>
+        </div>
+      </div>
     </div>
   );
 };
