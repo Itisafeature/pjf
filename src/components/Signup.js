63eb886f320b0e8f619b46ae2f72c0227af85e00
Itisafeature <thebigdebugger@gmail.com>
2020-09-07 23:49:03 -0400

    added signup functionality

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Signup.scss';
+import axios from 'axios';
 
 const Signup = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
+  const handleSignup = async () => {
+    try {
+      const res = await axios.post('/users/signup', {
+        username,
+        email,
+        password,
+        passwordConfirm,
+      });
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="signup-container">
       <div className="signup-welcome">
@@ -20,21 +40,41 @@ const Signup = () => {
       <div className="signup-information">
         <div className="signup-card">
           <h2 className="signup-card-header">Signup</h2>
-          <span className="signin-link-span signup-card-item">
+          <span className="signin-link-span">
             or <a href="#">Signin</a>
           </span>
-          <form className="signup-form">
+          <form className="signup-form" onSubmit={handleSignup}>
+            <input
+              type="text"
+              className="signup-card-item"
+              id="signup-username"
+              placeholder="username"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
             <input
               type="text"
               className="signup-card-item"
               id="signup-email"
               placeholder="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
             />
             <input
               type="password"
               className="signup-card-item"
               id="signup-password"
               placeholder="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <input
+              type="password"
+              className="signup-card-item"
+              id="signup-passwordConfirm"
+              placeholder="password confirmation"
+              value={passwordConfirm}
+              onChange={e => setPasswordConfirm(e.target.value)}
             />
             <button className="btn signup-form-btn" type="submit">
               Sign Up
